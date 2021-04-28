a2ca0e6865add8c4f22aaa954336f7cbc26d3d99
Arifulin Robert <robertarif2015@gmail.com>
2021-04-29 00:24:31 +0300

    mini refactoring + bug fixed

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ manager = pgui.UIManager((tp.WIDTH, tp.HEIGHT))
 screen = pygame.display.set_mode((tp.WIDTH, tp.HEIGHT))
 pygame.display.set_caption("Генератор загадок Эйнштейна")
 clock = pygame.time.Clock()
-screen.fill(tp.white)
+screen.fill(tp.WHITE)
 
-start_b = pgui.elements.UIButton(relative_rect=pygame.Rect((125, 20), tp.button_size),
+
+start_b = pgui.elements.UIButton(relative_rect=pygame.Rect((125, 20), tp.BUTTON_SIZE),
                                  text='Начать',
                                  manager=manager)
 
-width_input = pgui.elements.UITextEntryLine(relative_rect=pygame.Rect((200, 90), tp.input_size),
+width_input = pgui.elements.UITextEntryLine(relative_rect=pygame.Rect((200, 90), tp.INPUT_SIZE),
                                             manager=manager)
-height_input = pgui.elements.UITextEntryLine(relative_rect=pygame.Rect((200, 160), tp.input_size),
+height_input = pgui.elements.UITextEntryLine(relative_rect=pygame.Rect((200, 160), tp.INPUT_SIZE),
                                              manager=manager)
 
-width_input.set_allowed_characters(tp.white_list)
-height_input.set_allowed_characters(tp.white_list)
+width_input.set_allowed_characters(tp.WHITE_LIST)
+height_input.set_allowed_characters(tp.WHITE_LIST)
 width_input.set_text_length_limit(2)
 height_input.set_text_length_limit(2)
 
@@ -34,53 +35,67 @@ def start_bf():
 
 
 def generate_ans(width, height):
-    """Генерируем заполненую таблицу"""
+
+    """Генерируем заполненую таблицу."""
+
     table = [[] for _ in range(height)]
-    for i in range(width - 1):
-        letters = [i for i in tp.letters[:width]]
-        table[i] = [[f'{i + 1}{letters.pop(random.randint(1, len(letters)) - 1)}'] for _ in range(width)]
+    for i in range(height):
+        letters = [j for j in tp.LETTERS[:width]]  # Создаем новый массив букв
+        table[i] = [[f'{i + 1}{letters.pop(random.randint(0, len(letters) - 1))}'] for _ in range(width)]  # Заполняем строки параметрами НОМЕР_СТРОКИ+БУКВА
+
     completed_cells = []
-    for x in range(width):
+    for x in range(width):  # Создаем список нетронутых клеток
         completed_cells.append([])
         for y in range(height):
             completed_cells[-1].append(y)
 
+    print(table)
+    print(completed_cells)
     return table, completed_cells
 
 
-def check_possibility(completed_cells):  # переделать на проверку каждого типа
+def check_possibility(completed_cells):
+
+    """Проверка на возможность использования разных типов утверждений."""
+
+    """
+    Доступные типы утверждений:
+    000-только 1 тип
+    100-только 2 тип
+    010-только 3 тип
+    110-2 и 3 тип
+    011-3 и 4 тип
+    111-все типы (кроме 1)
+    """
+
     n = 0
     result = '000'
-
-    """ Доступные типы утверждений:
-        000-только 1 тип
-        100-только 2 тип
-        010-только 3 тип
-        110-2 и 3 тип
-        011-3 и 4 тип
-        111-все типы (кроме 1)"""
-
     for i in range(len(completed_cells)):
-        if len(completed_cells[i]) >= 2:  # 2 тип
+        if len(completed_cells[i]) >= 2:  # Проверка 2 типа
             result = '1' + result[1:]
             n += len(completed_cells[i])
 
-        if i and completed_cells[i] and completed_cells[i - 1]:  # 3 тип
+        if i and completed_cells[i] and completed_cells[i - 1]:  # Проверка 3 типа
             result = result[:1] + '1' + result[2:]
             n += len(completed_cells[i])
 
     if n < 3:
         return '000'
+
     return result
 
 
 def create_condition(possibility, table, completed_cells):
+
+    """Генерация новых утверждений."""
+
     height = len(table)
     width = len(table[0])
     new_condition = []
     new_question = ''
     new_answer = ''
-    if '1' in possibility:
+
+    if '1' in possibility:  # Если доступны типы с 2 по 4.
         roll = random.random()
         if roll >= 0.25 and int(possibility[0]) or possibility == '100':
             x = random.randint(0, width - 1)
@@ -89,8 +104,8 @@ def create_condition(possibility, table, completed_cells):
             y2 = completed_cells[x].pop(random.randint(0, len(completed_cells[x]) - 1))
             y1 = completed_cells[x][random.randint(0, len(completed_cells[x]) - 1)]
             k1, k2 = table[y1][x][0], table[y2][x][0]
-            new_condition = f'{k1} вместе с {k2}'
-            new_question = f'Что в доме {x + 1} под параметром {y2 + 1}?'
+            new_condition = f'2;{k1};{k2}'
+            new_question = f'{x + 1};{y2 + 1}'
             new_answer = k2
             for i in range(width):
                 if k2 not in table[y2][i]:
@@ -119,13 +134,14 @@ def create_condition(possibility, table, completed_cells):
             else:
                 place = 'слева'
             k1, k2 = table[y1][x1][0], table[y2][x2][0]
-            new_condition = f'дом с {k1} {place} от дома с {k2}'
-            new_question = f'Что в доме {x2 + 1} под параметром {y2 + 1}?'
+            new_condition = f'3;{k1};{place};{k2}'
+            new_question = f'{x2 + 1};{y2 + 1}'
             new_answer = k2
             for i in range(width):
                 if k2 not in table[y2][i]:
                     table[y2][i].append(k2)
-    else:
+
+    else:  # Если доступен только 1 тип.
         go = True
         while go:
             x = random.randint(0, width - 1)
@@ -133,7 +149,8 @@ def create_condition(possibility, table, completed_cells):
                 x = random.randint(0, width - 1)
             y = completed_cells[x].pop(random.randint(0, len(completed_cells[x]) - 1))
             k = table[y][x][0]
-            new_condition.append(f'дом {x + 1} c {k}')
+            new_condition.append(f'1;{x + 1};{k}')
+            new_question = f'{x + 1};{y + 1}'
             print(new_condition)
             for i in range(width):
                 if k not in table[y][i]:
@@ -144,6 +161,7 @@ def create_condition(possibility, table, completed_cells):
                 if i:
                     print(i, completed_cells)
                     go = True
+
         print(completed_cells)
         print(table)
         print(possibility)
@@ -155,19 +173,60 @@ def create_condition(possibility, table, completed_cells):
     return table, completed_cells, [new_condition], new_question, new_answer
 
 
+def convert(input):
+
+    """Делает вывод красивым."""
+
+    parts = input.split(';')
+    if len(parts) == 2 and parts[1][-1].isalpha():
+        return f'В доме с номером {parts[0]} параметр {parts[1]}.'  # Вывод ответа
+    elif len(parts) == 2 and not parts[1][-1].isalpha():
+        return f'Что в доме с номером {parts[0]} в параметре {parts[1]}?'  # Вывод вопроса
+    elif parts[0] == '0':
+        return f'В ряд стоят {parts[1]} дома(ов) с {parts[2]} параметрами \n.'  # Вывод условия
+    elif parts[0] == '1':
+        return f'В доме номер {parts[1]} параметр {parts[2]}.'  # Вывод 1 типа
+    elif parts[0] == '2':
+        return f'В доме, который {parts[1]}, {parts[2]}.'  # Вывод 2 типа
+    elif parts[0] == '3':
+        return f'Дом, который {parts[1]} {parts[2]} от дома, который {parts[3]}.'  # Вывод 3 типа
+
+
+
 def save_result(conditions, questions, answers):
+
+    """Сохранение результатов в файл."""
+
     file = open('Задачи\Условие.txt', 'w')
     for i in conditions:
-        file.write(i + '\n')
+        file.write(convert(i) + '\n')
     file.write('\n')
+
     for i in questions:
-        file.write(i + '\n')
+        file.write(convert(i) + '\n')
+    file.close()
+
+    file = open('Задачи\Ответ.txt', 'w')
+    for i in questions:
+        file.write(convert(i) + '\n')
+    file.write('\nОтвет: \n')
+
+    for i in answers:
+        file.write(convert(i) + '\n')
     file.close()
 
 
+def solve_puzzle(conditions, question, table):
+    pass
+
+
+
 def generate_puzzle():
+
+    """Основное тело программы."""
+
     table, competed_cells = generate_ans(4, 3)
-    conditions = [f'{len(table[0])} дома с {len(table)} параметрами \n']
+    conditions = [f'0;{len(table[0])};{len(table)}']
     questions = []
     answers = []
     possibility = '110'
@@ -178,11 +237,8 @@ def generate_puzzle():
             conditions.append(i)
         if len(questions) < 2:
             questions.append(new_question)
-            answers.append(new_answer)
+            answers.append(f'{new_question.split(";")[1]};{new_answer}')
     save_result(conditions, questions, answers)
-    print(conditions)
-    print(questions)
-    print(answers)
 
 
 generate_puzzle()
--- uTypes.py
+++ uTypes.py
@@ -3,15 +3,15 @@ WIDTH = 500  # ширина игрового окна
 HEIGHT = 700  # высота игрового окна
 FPS = 60  # частота кадров в секунду
 
-button_size = (250, 50)
-input_size = (50, 50)
+BUTTON_SIZE = (250, 50)
+INPUT_SIZE = (50, 50)
 
-white_list = [i for i in string.digits]
+WHITE_LIST = [i for i in string.digits]
 
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
-letters = string.ascii_letters
+LETTERS = string.ascii_letters
